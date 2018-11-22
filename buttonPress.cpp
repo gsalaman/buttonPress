d16061077aa3ba1e3c32be0be29111fc29838301
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-11-22 08:51:29 -0700

    Initial implementation

--- buttonPress.cpp
+++ buttonPress.cpp
@@ -1,1 +1,34 @@
+#include "arduino.h"
 #include "buttonPress.h"
+
+ButtonPress::ButtonPress(int pin)
+{
+    _pin = pin;
+}
+
+void ButtonPress::begin( void )
+{
+    pinMode(_pin, INPUT_PULLUP);
+    _lastState = HIGH;
+}
+
+bool ButtonPress::pressed( void )
+{
+    int  currentState;
+    bool pressed;
+
+    currentState = digitalRead(_pin);
+    
+    if ((currentState == LOW) && (_lastState == HIGH))
+    {
+	pressed = true;
+    }
+    else
+    {
+	pressed = false;
+    }
+
+    _lastState = currentState;
+    return(pressed);
+}
+
--- buttonPress.h
+++ buttonPress.h
@@ -1,10 +1,15 @@
 #ifndef BUTTON_PRESS_H
 #define BUTTON_PRESS_H
 
-
-
-
-
-
+class ButtonPress
+{
+    public:
+	ButtonPress(int pin);
+	void begin( void );
+	bool pressed( void );
+    private:
+        int _pin;
+	int _lastState;
+};	
 
 #endif // BUTTON_PRESS_H
